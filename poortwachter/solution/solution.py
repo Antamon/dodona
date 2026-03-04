76404994a1a32442d8c4abd02713b65409460b04
Antamon <jan.mertens@so.antwerpen.be>
2026-03-04 16:48:35 +0100

    Add solution.py for Poortwachter exercise

---
+++ poortwachter/solution/solution.py
@@ -0,0 +1,20 @@
+# Oefening 1 - Modeloplossing: De Poortwachter van Codeholm
+
+wachtwoord = input("Geef het wachtwoord: ")
+code = int(input("Geef je badgecode (geheel getal): "))
+controle = int(input("Geef het controlegetal (geheel getal): "))
+
+verwacht_wachtwoord = "AETHER"
+verwacht_badge = 407
+
+# berekening controlegetal volgens de regel
+verwacht_controle = (badge // 10) + (badge % 10)
+
+if wachtwoord != verwacht_wachtwoord:
+    print("Fout wachtwoord.")
+elif controle != verwacht_controle:
+    print("Controlegetal klopt niet.")
+elif code != verwacht_badge:
+    print("Ongeldige badge.")
+else:
+    print("Toegang verleend. De poort schuift open.")
